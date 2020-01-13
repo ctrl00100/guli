5cdcbc969ba487fcaa283e93afaa6cd70ce4d552
Admin <k1772714@163.com>
2020-01-13 23:27:45 +0800

    mp非物理删除,锁

---
+++ guli_teacher/src/main/java/com/guli/teacher/TeacherApplication.java
@@ -0,0 +1,19 @@
+package com.guli.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2020/1/13 16:50
+ * @Description:
+ */
+
+@SpringBootApplication
+public class TeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherApplication.class, args);
+    }
+
+}
