24bfb6c6200079162e45a42e389ca5797668c23a
Admin <k1772714@163.com>
2020-01-20 22:29:54 +0800

    course

---
+++ guli_teacher/src/main/java/com/guli/teacher/controller/EduCourseController.java
@@ -0,0 +1,21 @@
+package com.guli.teacher.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程 前端控制器
+ * </p>
+ *
+ * @author guli
+ * @since 2020-01-20
+ */
+@RestController
+@RequestMapping("/teacher/edu-course")
+public class EduCourseController {
+
+}
+
---
+++ guli_teacher/src/main/java/com/guli/teacher/entity/vo/CourseVo.java
@@ -0,0 +1,15 @@
+package com.guli.teacher.entity.vo;
+
+import com.guli.teacher.entity.EduChapter;
+import com.guli.teacher.entity.EduCourse;
+import com.guli.teacher.entity.EduCourseDescription;
+import lombok.Data;
+
+@Data
+public class CourseVo {
+
+    private EduCourse eduCourse;
+
+    private EduCourseDescription courseDescription;
+
+}
